705a0788be0b6c4dd3c8a15b9942e8ea5b75888f
stepan881 <stepanstepanov180@gmail.com>
2021-01-14 17:16:50 +0300

    add program

--- app/js/main.js
+++ app/js/main.js
@@ -12,3 +12,18 @@ navBtnclose.addEventListener(`click`, evt => {
   menu.classList.remove(`nav--open`)
 })
 
+const programItems = document.querySelectorAll(`.program__item`);
+
+programItems.forEach((el, i) => {
+  let title = el.querySelector(`.program__title`);
+  let text = el.querySelector(`.program__text`);
+  
+  if (i === 1) {
+    title.innerHTML = title.innerHTML.trim().replace(/(.{2})/, `<span class="yellow">$1</span>`);
+    text.innerHTML = text.innerHTML.trim().replace(/(.{3})/, `<span class="white">$1</span>`);
+  } else if (i === 2) {
+    title.innerHTML = title.innerHTML.trim().replace(/(.{6})/, `<span class="yellow">$1</span>`);
+    text.innerHTML = text.innerHTML.trim().replace(/(.{4})/, `<span class="white">$1</span>`);
+  }
+})
+
